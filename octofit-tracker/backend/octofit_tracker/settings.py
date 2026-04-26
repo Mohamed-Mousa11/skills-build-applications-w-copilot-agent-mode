92242d832069144fd489254b4a20ce7fdbac9382
Mohamed Mousa <167845682+Mohamed-Mousa11@users.noreply.github.com>
2026-04-26 10:16:08 +0000

    n

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -26,8 +26,12 @@ SECRET_KEY = 'django-insecure-y$x&ut+99x%lzsf0%yl=mi#(&005l&d_gty0-j+jhbsee^$%bi
 DEBUG = True
 
 
-# Allow all hosts
-ALLOWED_HOSTS = ['*']
+# Allow codespace and localhost
+import os
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME')
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if CODESPACE_NAME:
+    ALLOWED_HOSTS.append(f'{CODESPACE_NAME}-8000.app.github.dev')
 
 
 # Application definition
